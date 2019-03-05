a58717978d1785deb0347b618cff6ed11e4b379c
melvinantony <melvinantony9168@gmail.com>
2019-03-05 15:30:15 -0800

    find largest

---
+++ large.py
@@ -0,0 +1,14 @@
+a=78
+b=87
+c=34
+print("the largest no: is")
+if(a>b):
+    if(a>c):
+        print(a)
+    else:
+        print(c)
+else:
+    if(b>c):
+        print(b)
+    else:
+        print(c)
